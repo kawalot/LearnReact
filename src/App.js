ebbc259a3d85d75904cb8688ed2c3d96077f67ea
AleksandrPeshcherov <kawalot@gmail.com>
2019-03-14 11:54:48 +0200

    try to change component state

--- src/App.js
+++ src/App.js
@@ -4,25 +4,52 @@ import './App.css';
 import TodoItem from './components/TodoItem'
 import todosData from './todosData'
 
-class App extends Component {
+
+class App extends React.Component {
   constructor() {
-    super()
-    this.state = {
-      todos: todosData
-    }
+      super()
+      this.state = {
+          count: 0
+      }
+      this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick() {
+    this.setState(prevState => {
+      return {
+        count: prevState.count + 1
+      }
+    })
   }
   render() {
-    const todos = this.state.todos.map(
-      item => <TodoItem key={item.id} item={item} />
-    )
-    return (
-      <div className="todo-list">
-
-        {todos}
-
-      </div>
-    );
+      return (
+          <div>
+              <h1>{this.state.count}</h1>
+              <button onClick={this.handleClick}>Change!</button>
+          </div>
+      )
   }
 }
+
+
+// class App extends Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       todos: todosData
+//     }
+//   }
+//   render() {
+//     const todos = this.state.todos.map(
+//       item => <TodoItem key={item.id} item={item} />
+//     )
+//     return (
+//       <div className="todo-list">
+
+//         {todos}
+
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,7 +3,8 @@ import React from "react"
 function TodoItem(props) {
     return (
         <div className="todo-item">
-            <p style={{color: !props.item.completed && "#888"}}><input type="checkbox" checked={props.item.completed}/>{props.item.text}</p>
+            <p style={{color: !props.item.completed && "#888"}}>
+            <input type="checkbox" onChange={() => console.log("Changed!")} checked={props.item.completed}/>{props.item.text}</p>
         </div> 
     )
 };
