21a472a33de41875187ef1b15ab4b6962731865e
AleksandrPeshcherov <kawalot@gmail.com>
2019-03-14 16:56:18 +0200

    practice with conditional rendering

--- src/App.js
+++ src/App.js
@@ -5,41 +5,72 @@ import TodoItem from './components/TodoItem'
 import todosData from './todosData'
 
 
+import Conditional from "./components/Conditional"
+
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      todos: todosData
-    }
-    this.handleChange = this.handleChange.bind(this)
-  }
-
-  handleChange(id) {
-    console.log('changed', id)
-    this.setState(prevState => {
-      const updatedTodos = prevState.todos.map(todo => {
-        if (todo.id === id) {
-          todo.completed = !todo.completed
+    constructor() {
+        super()
+        this.state = {
+            isLoading: true
         }
-        return todo
-      })
-      return {
-        todos: updatedTodos
-      }
-    })
-  }
-  render() {
-    const todos = this.state.todos.map(
-      item => <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
-    )
-    return (
-      <div className="todo-list">
-
-        {todos}
-
-      </div>
-    );
-  }
+    }
+    
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                isLoading: false
+            })
+        }, 1500)
+    }
+    
+    render() {
+        return (
+            <div>
+              {this.state.isLoading ?
+              <h1>Loading...</h1> :
+              <Conditional />}
+            </div>
+        )
+    }
 }
+
+
+
+// class App extends Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       todos: todosData
+//     }
+//     this.handleChange = this.handleChange.bind(this)
+//   }
+
+//   handleChange(id) {
+//     console.log('changed', id)
+//     this.setState(prevState => {
+//       const updatedTodos = prevState.todos.map(todo => {
+//         if (todo.id === id) {
+//           todo.completed = !todo.completed
+//         }
+//         return todo
+//       })
+//       return {
+//         todos: updatedTodos
+//       }
+//     })
+//   }
+//   render() {
+//     const todos = this.state.todos.map(
+//       item => <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
+//     )
+//     return (
+//       <div className="todo-list">
+
+//         {todos}
+
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
