38cb660b3ca23351646772a470031ef732eaa2a7
AleksandrPeshcherov <kawalot@gmail.com>
2019-03-26 22:55:21 +0200

    try to handle some input forms in react

--- src/App.js
+++ src/App.js
@@ -9,14 +9,17 @@ class App extends Component {
         super()
         this.state = {
             firstName: "",
-            lastName: ""
+            lastName: "",
+            isFriendly: true,
+            gender: "",
+            favColor: "blue"
         }
         this.handleChange = this.handleChange.bind(this)
     }
 
     handleChange(event) {
-        const {name, value} = event.target
-        this.setState({[name]: value})
+        const {name, value, type, checked} = event.target
+        type === "checkbox" ? this.setState({[name]: checked}) : this.setState({[name]: value})
     }
 
     render() {
@@ -37,7 +40,55 @@ class App extends Component {
                            onChange={this.handleChange}
                     />
                 <h1>{this.state.firstName} {this.state.lastName}</h1>
+
+                <textarea
+                    value="some value"
+                    onChange={this.handleChange}
+                />
+
+                <label>
+                    <input
+                        type="checkbox"
+                        checked={this.state.isFriendly}
+                        name="isFriendly"
+                        onChange={this.handleChange}
+                    />
+                is friendly?
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        name="gender"
+                        value="male"
+                        checked={this.state.gender === "male"}
+                        onChange={this.handleChange}
+                    /> Male
+                </label>
+                <br />
+                <label>
+                    <input
+                        type="radio"
+                        name="gender"
+                        value="female"
+                        checked={this.state.gender === "female"}
+                        onChange={this.handleChange}
+                    /> Female
+                </label>
                 </form>
+                <h2><font color="#3AC1EF">You are a {this.state.gender}</font></h2>
+                    <label>Favorite Color:</label>
+                    <select
+                        value={this.state.favColor}
+                        onChange={this.handleChange}
+                        name="favColor"
+                    >
+                        <option value="blue">Blue</option>
+                        <option value="green">Green</option>
+                        <option value="red">Red</option>
+                        <option value="orange">Orange</option>
+                        <option value="yellow">Yellow</option>
+                    </select>
+                    <h2><font color="#3AC1EF">Your favorite color is {this.state.favColor}</font></h2>
             </div>
         </div>)
     }
